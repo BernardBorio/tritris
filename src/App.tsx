86a192a30df858437e8db5d92108d54717a47d51
Bernard Borio <bernardborio@gmail.com>
2023-12-23 21:11:10 +0100

    Firebase integration

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,25 @@ import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import Board from "./components/Board";
+import {getDatabase, ref, set, onValue} from "firebase/database";
 
-function App() {
+function App(props: any) {
 
 	const [screenSize, setScreenSize] = useState(getCurrentDimension());
-
+	const firebase = props.app
 	function getCurrentDimension() {
 		return {
 			width: window.innerWidth,
 			height: window.innerHeight
 		}
 	}
+
+	const db = getDatabase(firebase);
+
+	const data = ref(db, 'matches');
+	onValue(data, (snapshot) => {
+		console.log(snapshot.val());
+	})
 
 	useEffect(() => {
 		const updateDimension = () => {
--- src/index.tsx
+++ src/index.tsx
@@ -7,14 +7,33 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { initializeApp } from "firebase/app";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCxR-6J_ZBy-epu55GwHzPlM-psWVrRS9g",
+  authDomain: "supertris-33fb9.firebaseapp.com",
+  projectId: "supertris-33fb9",
+  storageBucket: "supertris-33fb9.appspot.com",
+  messagingSenderId: "40641633957",
+  appId: "1:40641633957:web:29acdf369f6373df8a374c",
+  databaseURL: "https://supertris-33fb9-default-rtdb.europe-west1.firebasedatabase.app/"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App app={app}/>
   </React.StrictMode>
 );
 
