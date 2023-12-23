3f9b81adb338cde7c4da8487f723ffb1ac281f07
Bernard Borio <bernardborio@gmail.com>
2023-12-23 17:50:21 +0100

    Basic funtionalities done

--- src/App.tsx
+++ src/App.tsx
@@ -5,33 +5,9 @@ import Board from "./components/Board";
 
 function App() {
 
-	const [cellsContent, setCellsContent] = useState<string[][]>([...Array(9)].map(() => [...Array(9)].map(() => '')));
-	const [turn, setTurn] = useState<'X' | 'O'>('X');
-
-	function selectCell(i: number, j: number) {
-		setCellsContent([...cellsContent].map((row, rowIndex) => {
-			return row.map((cell, cellIndex) => {
-				if (rowIndex === i && cellIndex === j) {
-					if (turn === 'X') {
-						setTurn('O')
-						return 'X'
-					}
-					if (turn === 'O') {
-						setTurn('X')
-						return 'O'
-					}
-				}
-				return cell
-			})
-		}))
-	}
-
 	return (
 		<div className={"container"}>
 			<Board
-				cellsContent={cellsContent}
-				selectCell={selectCell}
-				turn={turn}
 			/>
 		</div>
 
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,37 +1,170 @@
-import React from "react";
-import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
-import PanoramaFishEyeIcon from '@mui/icons-material/PanoramaFishEye';
+import React, {useEffect, useState} from "react";
 import {ReactComponent as Circle} from '../assets/circle.svg';
 import {ReactComponent as Cross} from '../assets/cross.svg';
 
 export default function Board(props: any) {
+
+	const [cellsContent, setCellsContent] = useState<string[][]>([...Array(9)].map(() => [...Array(9)].map(() => '')));
+	const [turn, setTurn] = useState<'X' | 'O'>('X');
+	const [cellsStatus, setCellsStatus] = useState<boolean[]>([...Array(9)].map(() => true));
+	const [macroCellsContent, setMacroCellsContent] = useState<string[]>([...Array(9)].map(() => ''));
+	const [cellToCheck, setCellToCheck] = useState<number>(0);
+
+	useEffect(() => {
+		checkMicroTris(cellToCheck)
+	}, [cellsContent]);
+
+	function selectCell(i: number, j: number) {
+		setCellToCheck(i)
+		setCellsContent([...cellsContent].map((row, rowIndex) => {
+			return row.map((cell, cellIndex) => {
+				if (rowIndex === i && cellIndex === j) {
+					if (turn === 'X') {
+						setTurn('O')
+						return 'X'
+					}
+					if (turn === 'O') {
+						setTurn('X')
+						return 'O'
+					}
+				}
+				return cell
+			})
+		}))
+		console.log('selectCell', i, j)
+		console.log('cellsContent', cellsContent)
+		if (macroCellsContent[j] !== '') {
+			setCellsStatus([...cellsStatus].map((cell, index) => {
+				return true;
+			}))
+		}
+		else {
+			setCellsStatus([...cellsStatus].map((cell, index) => {
+				return index === j;
+			}))
+		}
+	}
+
+	function checkMicroTris(i: number) {
+		console.log('checkMicroTris', i)
+		if (cellsContent[i][0] === cellsContent[i][1] && cellsContent[i][0] === cellsContent[i][2] && cellsContent[i][0] !== '') {
+			setMacroCellsContent([...macroCellsContent.map((c, index) => {
+				if (index === i) {
+					return cellsContent[i][0]
+				}
+				return c
+			})])
+			setCellsStatus([...cellsStatus.map(()=>{return true})])
+		}
+		if (cellsContent[i][3] === cellsContent[i][4] && cellsContent[i][3] === cellsContent[i][5] && cellsContent[i][3] !== '') {
+			setMacroCellsContent([...macroCellsContent.map((c, index) => {
+				if (index === i) {
+					return cellsContent[i][3]
+				}
+				return c
+			})])
+			setCellsStatus([...cellsStatus.map(()=>{return true})])
+		}
+		if (cellsContent[i][6] === cellsContent[i][7] && cellsContent[i][6] === cellsContent[i][8] && cellsContent[i][6] !== '') {
+			setMacroCellsContent([...macroCellsContent.map((c, index) => {
+				if (index === i) {
+					return cellsContent[i][6]
+				}
+				return c
+			})])
+			setCellsStatus([...cellsStatus.map(()=>{return true})])
+		}
+		if (cellsContent[i][0] === cellsContent[i][3] && cellsContent[i][0] === cellsContent[i][6] && cellsContent[i][0] !== '') {
+			setMacroCellsContent([...macroCellsContent.map((c, index) => {
+				if (index === i) {
+					return cellsContent[i][0]
+				}
+				return c
+			})])
+			setCellsStatus([...cellsStatus.map(()=>{return true})])
+		}
+		if (cellsContent[i][1] === cellsContent[i][4] && cellsContent[i][1] === cellsContent[i][7] && cellsContent[i][1] !== '') {
+			setMacroCellsContent([...macroCellsContent.map((c, index) => {
+				if (index === i) {
+					return cellsContent[i][1]
+				}
+				return c
+			})])
+			setCellsStatus([...cellsStatus.map(()=>{return true})])
+		}
+		if (cellsContent[i][2] === cellsContent[i][5] && cellsContent[i][2] === cellsContent[i][8] && cellsContent[i][2] !== '') {
+			setMacroCellsContent([...macroCellsContent.map((c, index) => {
+				if (index === i) {
+					return cellsContent[i][2]
+				}
+				return c
+			})])
+			setCellsStatus([...cellsStatus.map(()=>{return true})])
+		}
+		if (cellsContent[i][0] === cellsContent[i][4] && cellsContent[i][0] === cellsContent[i][8] && cellsContent[i][0] !== '') {
+			setMacroCellsContent([...macroCellsContent.map((c, index) => {
+				if (index === i) {
+					return cellsContent[i][0]
+				}
+				return c
+			})])
+			setCellsStatus([...cellsStatus.map(()=>{return true})])
+		}
+		if (cellsContent[i][3] === cellsContent[i][4] && cellsContent[i][3] === cellsContent[i][6] && cellsContent[i][3] !== '') {
+			setMacroCellsContent([...macroCellsContent.map((c, index) => {
+				if (index === i) {
+					return cellsContent[i][3]
+				}
+				return c
+			})])
+			setCellsStatus([...cellsStatus.map(()=>{return true})])
+		}
+	}
+
 	return (
 		<div className={"board"}>
 			{[...Array(9)].map((_, i) => {
 				return (
-					<div className={"tile"} key={i}>
-						<div className={`subTileContainer`}>
-							{[...Array(9)].map((_, j) => {
-								return (
-									<div
-										onClick={() => props.selectCell(i, j)}
-										id={`${i}${j}`}
-										className={
-											`subTile 
-											${props.cellsContent[i][j] !== '' ? 'disabled' : ''}
-											${props.turn === 'X' ? 'red' : 'blue'}`
-										} key={j}>
-										{
-											props.cellsContent[i][j] === '' ? '' :
-												props.cellsContent[i][j] === 'X' ?
-													<Cross fill={"#a21b1b"} width={"50%"} height={"50%"}/>
-													:
-													<Circle stroke={"#001CBE"} width={"calc(50% + 2px)"} height={"calc(50% + 2px)"}/>
-										}
-									</div>
-								)
-							})}
-						</div>
+					<div className={`
+							tile 
+							${cellsStatus[i] ? '' : 'disabled'}
+							${macroCellsContent[i] === '' ? '' : 'filled'}`
+						} key={i}>
+						{
+							macroCellsContent[i] === '' ?
+								<div className={`subTileContainer`}>
+									{[...Array(9)].map((_, j) => {
+										return (
+											<div
+												onClick={() => selectCell(i, j)}
+												id={`${i}${j}`}
+												className={
+													`subTile 
+													${cellsContent[i][j] !== '' ? 'filled' : ''}
+													${turn === 'X' ? 'red' : 'blue'}
+													${cellsStatus[i] ? '' : 'disabled'}`
+												} key={j}>
+												{
+													cellsContent[i][j] === '' ? '' :
+														cellsContent[i][j] === 'X' ?
+															<Cross fill={"#a21b1b"} width={"50%"} height={"50%"}/>
+															:
+															<Circle stroke={"#001CBE"} width={"calc(50% + 2px)"} height={"calc(50% + 2px)"}/>
+												}
+											</div>
+										)
+									})}
+								</div>
+								:
+								<div className={`tileWinner`}>
+									{
+										macroCellsContent[i] === 'X' ?
+											<Cross fill={"#a21b1b"} width={"100%"} height={"100%"}/>
+											:
+											<Circle stroke={"#001CBE"} width={"calc(100% + 2px)"} height={"calc(100% + 2px)"}/>
+									}
+								</div>
+						}
 					</div>)
 			})}
 		</div>
